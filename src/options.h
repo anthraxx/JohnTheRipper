openwall/john

Benchmark using mask mode (and possibly GPU-side acceleration), unless new option --no-mask is given along with --test.  Closes #3697

src/options.h
@@ -190,6 +190,7 @@
 #define FLG_SUBSETS_CHK			0x0800000000000000ULL
 #define FLG_SUBSETS_SET \
 	(FLG_SUBSETS_CHK | FLG_CRACKING_SET)
+#define FLG_NO_MASK_BENCH		0x1000000000000000ULL
 
 /*
  * Macro for getting correct node number regardless of if MPI or not

anthraxx/JohnTheRipper

src/options.h
@@ -195,6 +195,7 @@
 #define FLG_RULES_IN_USE		0x2000000000000000ULL
 /* Ignore NOP rules */
 #define FLG_RULE_SKIP_NOP		0x4000000000000000ULL
+#define FLG_NO_MASK_BENCH		0x8000000000000000ULL
 
 /*
  * Macro for getting correct node number regardless of if MPI or not